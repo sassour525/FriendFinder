c8c7587152426c44a2d07540813b39d13d18f975
Shawn Assour <sassour@Shawns-MacBook-Pro.local>
2017-04-19 17:15:07 -0500

    all logic in place - currently a working app

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,19 +1,38 @@
 // Data Routes
 var path = require("path");
+
+//array to hold each user of the site
+var users = [];
+//export the paths to be used in the server.js file
 module.exports = function(app) {
 
-	app.get("/api/friends", function(res, req) {
-		return res.json(friends);
+	//serve user objects
+	app.get("/api/friends", function(req, res) {
+		return res.json(users);
 	});
 
-	app.post("/api/friends", function(res, req) {
-		var userRes = req.body;
-		var newUser = new User(userRes.name, userRes.photo, userRes.scores);
+	//post data from survey entries / calculate match
+	app.post("/api/friends", function(req, res) {
+		var userReq = req.body;
+		console.log(userReq);
 
+		//user constructor
+		function User(name, photo, scores) {
+			this.name = name;
+			this.photo = photo;
+			this.scores = scores;
+		};
+
+		//use constructor to create new user
+		var newUser = new User(userReq.name, userReq.photo, userReq.scores);
+
+		//add new entry to users array
 		users.push(newUser);
 
+		//array used to calculate match
 	    var totalArray = [];
 
+	    //loop through users array and compare scores to the new user
 		for (var i = 0; i < users.length; i++) {
 			var totalDifference = 0;
 			for(var j = 0; j < newUser.scores.length; j++) {
@@ -26,15 +45,19 @@ module.exports = function(app) {
 		}
 
 		console.log("Total Array: " + totalArray);
+		//remove last entry in the array so you do not match with yourself
+		totalArray.pop();
 
+		//function to find minium value in array
 		Array.min = function(array){
 			return Math.min.apply(Math, array);
 		};
 
+		//index of the minimum entry to serve to modal
 		var minimum = totalArray.indexOf(Array.min(totalArray));
 		console.log(minimum);
 
+		//return user object at the index location
 		res.json(users[minimum]);
 	});
-
 }
--- server.js
+++ server.js
@@ -7,22 +7,18 @@ var apiRoutes = require("./app/routing/apiRoutes.js");
 var app = express();
 var PORT = 3000;
 
+//used to parse incomming requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-var users = [];
-
+//get HTML routes
 htmlRoutes(app);
+//get API routes
 apiRoutes(app);
-
-function User(name, photo, scores) {
-	this.name = name;
-	this.photo = photo,
-	this.scores = scores;
-};
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
+
